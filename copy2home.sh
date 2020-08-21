46b1adc844660e6d4844263407dfcd04488083a8
bigyo-fourdesire <bigyo.tseng@fourdesire.com>
2020-08-21 15:26:01 +0800

    Add omnisharp

--- copy2home.sh
+++ copy2home.sh
@@ -1,3 +1,4 @@
 cp .gitconfig ~/.gitconfig
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
+cp omnisharp.json ~/.omnisharp/omnisharp.json
--- copyFromHome.sh
+++ copyFromHome.sh
@@ -1,3 +1,4 @@
 cp ~/.gitconfig .gitconfig
 cp ~/.zshrc .zshrc
 cp ~/.p10k.zsh .p10k.zsh
+cp ~/.omnisharp/omnisharp.json omnisharp.json
